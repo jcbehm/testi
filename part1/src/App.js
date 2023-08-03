49d23eaf47bb6dc509922d738d7f827802fc8133
Jami Behm <yompatchi@gmail.com>
2023-08-03 23:10:53 +0300

    torstai-illan testailuja

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Hello = (props) => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log("Muuttujat: ", now, a+b)
+  console.log("Parametrit: ", props)
+  return (
+    <div>
+      <br />
+      <p>Hei {props.name}, nyt on {now.toString()} ja sinä olet {props.age}-vuotias</p>
+      <p>
+        {a} plus {b} on {a + b}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const boss = {
+    name: 'Morticia',
+    age: 89
+  }
+  console.log('Hauska tutustua, selaimen konsoli. Minä olen React-komponentti.')
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Terveisiä React-maailmasta!
       </header>
+      <p>Heipähei, hyvää iltaa.</p>
+      <Hello name="Eevertti" age={29+4}/>
+      <Hello name="Pulmu-Ulpu" age={3*5-6}/>
+      <Hello name={boss.name} age={boss.age} />
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
